a5b4ce11f03264ccf12e3f70de6423408dae9690
Jake Stone <Stonedev12@gmail.com>
2021-08-31 18:20:11 -0700

    local storage stores view and allows page to maintain same view after refresh.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@
 /* exported data */
 
 /* THIS IS MY MAIN JAVASCRIPT FILE */
+/* Content loaded */
+document.addEventListener('DOMContentLoaded', function () {
+  loadEntries(data);
+  if (data.view === 'entries') {
+    showEntries();
+  } else {
+    data.view = 'entry-form';
+    showEntryForm();
+  }
+
+});
+
 /* This bit of code sets the image url to the match the valid url input of the user */
 var imgUrl = document.getElementById('photo');
-
 var placeholderImg = document.getElementById('placed-image');
-
 function validURL(str) {
   var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
@@ -16,7 +26,6 @@ function validURL(str) {
     '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
   return !!pattern.test(str);
 }
-
 imgUrl.addEventListener('input', function (e) {
   if (validURL(imgUrl.value)) {
     placeholderImg.src = imgUrl.value;
@@ -29,19 +38,17 @@ var newEntryId;
 var nextEntryId;
 var previousUserInput;
 var previousUserInputJSON = localStorage.getItem('javascript-local-storage');
-
 /* getting previous entry ID and incrementing */
 if (previousUserInputJSON !== null) {
   previousUserInput = JSON.parse(previousUserInputJSON);
   newEntryId = previousUserInput.nextEntryId;
   nextEntryId = previousUserInput.nextEntryId + 1;
-
   /* get prior data object */
   if (previousUserInput) {
     data.entries = previousUserInput.entries;
     data.nextEntryId = nextEntryId;
+    data.view = previousUserInput.view;
   }
-
 } else {
   newEntryId = 0;
   nextEntryId = 1;
@@ -50,12 +57,9 @@ if (previousUserInputJSON !== null) {
 /* getting form elements */
 var title = document.getElementById('title');
 var notes = document.getElementById('notes');
-
 /* adding event listener for submit and setting input values */
 document.addEventListener('submit', function (event) {
-
-  // event.preventDefault();
-
+  event.preventDefault();
   /* form object */
   var userInput = {
     title: '',
@@ -63,7 +67,6 @@ document.addEventListener('submit', function (event) {
     image: '',
     entryID: ''
   };
-
   userInput.title = title.value;
   userInput.notes = notes.value;
   userInput.image = imgUrl.value;
@@ -71,76 +74,108 @@ document.addEventListener('submit', function (event) {
 
   /* add new data to data object */
   data.entries.unshift(userInput);
-
   /* converting input values to json string and storing locally */
   var inputToJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', inputToJSON);
+
+  entriesPage.className = 'view-hidden';
+  inputForm.className = 'view';
+  /* takes current user input and turns it into dom element. Prepends it to page */
+  addEntry(userInput);
+  /* clear form input */
+  document.getElementById('form').reset();
+  placeholderImg.src = 'images/placeholder-image-square.jpg';
+  showEntries();
 });
 
-/* User can view their entries */
-function entryDOM(data) {
-
+/* Places dom elements on page load */
+function loadEntries(data) {
   /* Loops through stored data and uses data for new elements */
-  var entryContainer = document.getElementById('entry-list');
-
   for (var i = 0; i < data.entries.length; i++) {
     /* Creates new elements using DOM */
-    var newEntryRow = document.createElement('div');
-    newEntryRow.className = 'entries-row';
-
-    var newEntryCol = document.createElement('div');
-    newEntryCol.className = 'entries-col';
-
-    var imgEntryCol = document.createElement('div');
-    imgEntryCol.className = 'entries-col-img';
-
-    var dividerCol = document.createElement('div');
-    dividerCol.className = 'entries-divider-col';
-
-    var title = document.createElement('h2');
-    title.className = 'entries-title';
-    title.textContent = data.entries[i].title;
-
-    var imgURL = document.createElement('div');
-    imgURL.className = 'img-wrapper1';
-    imgURL.style.backgroundImage = 'url(' + data.entries[i].image + ')';
-
-    var entryNotes = document.createElement('p');
-    entryNotes.textContent = data.entries[i].notes;
-    entryNotes.className = 'entries-text';
-
-    /* appends the new elements to their parent element */
-    newEntryCol.appendChild(title);
-    newEntryCol.appendChild(entryNotes);
-    imgEntryCol.appendChild(imgURL);
-    newEntryRow.appendChild(imgEntryCol);
-    newEntryRow.appendChild(dividerCol);
-    newEntryRow.appendChild(newEntryCol);
-    var NewEntry = document.createElement('li');
-    NewEntry.className = 'entry';
-    NewEntry.appendChild(newEntryRow);
-    /* This container holds an entire new entry */
-    var placeholder = document.getElementById('no-entry');
-    placeholder.style.display = 'none';
-    entryContainer.appendChild(NewEntry);
+    addEntry(data.entries[i]);
   }
 }
 
-document.addEventListener('DOMContentLoaded', entryDOM(data));
+var entryContainer = document.getElementById('entry-list');
+/* This function turns entry data into a DOM element */
+function addEntry(entry) {
+  var newEntryRow = document.createElement('div');
+  newEntryRow.className = 'entries-row';
+
+  var newEntryCol = document.createElement('div');
+  newEntryCol.className = 'entries-col';
+
+  var imgEntryCol = document.createElement('div');
+  imgEntryCol.className = 'entries-col-img';
+
+  var dividerCol = document.createElement('div');
+  dividerCol.className = 'entries-divider-col';
+
+  var title = document.createElement('h2');
+  title.className = 'entries-title';
+  title.textContent = entry.title;
+
+  var imgURL = document.createElement('div');
+  imgURL.className = 'img-wrapper1';
+  imgURL.style.backgroundImage = 'url(' + entry.image + ')';
+
+  var entryNotes = document.createElement('p');
+  entryNotes.textContent = entry.notes;
+  entryNotes.className = 'entries-text';
+
+  /* appends the new elements to their parent element */
+  newEntryCol.appendChild(title);
+  newEntryCol.appendChild(entryNotes);
+  imgEntryCol.appendChild(imgURL);
+  newEntryRow.appendChild(imgEntryCol);
+  newEntryRow.appendChild(dividerCol);
+  newEntryRow.appendChild(newEntryCol);
+  var NewEntry = document.createElement('li');
+  NewEntry.className = 'entry';
+  NewEntry.appendChild(newEntryRow);
+  /* This container holds an entire new entry */
+  var placeholder = document.getElementById('no-entry');
+  placeholder.style.display = 'none';
+  /* Adds entry to front of entries */
+  entryContainer.prepend(NewEntry);
+}
 
 /* click event on anchor to change views to entries */
 var entriesTab = document.querySelector('.entries-anchor');
-var entriesList = document.querySelector('[data-view="entry-form"]');
-var entryForm = document.querySelector('[data-view="input-form"]');
+var entriesPage = document.querySelector('[data-view="entries"]');
+var inputForm = document.querySelector('[data-view="entry-form"]');
+
+/* click event on nav item for entries page view */
+function showEntries() {
+  data.view = 'entries';
+
+  inputForm.className = 'view-hidden';
+  entriesPage.className = 'view';
+
+  // Save view to local storage
+  var inputToJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', inputToJSON);
+
+}
 entriesTab.addEventListener('click', function () {
-  entryForm.className = 'view-hidden';
-  entriesList.className = 'view';
+  data.view = 'entries';
+  showEntries();
 });
 
-/* click event on new button to return to main */
-var newButton = document.getElementById('newEntryButton');
+/* click event on new button to return to input form */
+function showEntryForm() {
+  data.view = 'entry-form';
 
+  entriesPage.className = 'view-hidden';
+  inputForm.className = 'view';
+
+  // Save view to local storage
+  var inputToJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', inputToJSON);
+}
+
+var newButton = document.getElementById('newEntryButton');
 newButton.addEventListener('click', function () {
-  entriesList.className = 'view-hidden';
-  entryForm.className = 'view';
+  showEntryForm();
 });
