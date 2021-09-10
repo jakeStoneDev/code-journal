68fedaff06f81c42e95f6abeb218201dcc83f8d0
Jake Stone <Stonedev12@gmail.com>
2021-09-09 21:11:29 -0700

    Complete issue #3.

--- js/main.js
+++ js/main.js
@@ -3,28 +3,22 @@
 
 /* Content loaded and view set */
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOMContentLoaded');
   loadEntries(data);
 
-  console.log('data.view', data.view);
   if (data.view === 'entries') {
     showEntries();
   } else {
     data.view = 'entry-form';
     showEntryForm();
   }
-  console.log('data.view', data.view);
 });
 
 /* Sets img on entry form before submit */
 var imgUrl = document.getElementById('photo');
-console.log('imgUrl', imgUrl);
 
 var placeholderImg = document.getElementById('placed-image');
-console.log('placeholderImg', placeholderImg);
 
 function validURL(str) {
-  console.log('validURL()');
 
   var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
@@ -35,25 +29,39 @@ function validURL(str) {
   return !!pattern.test(str);
 }
 imgUrl.addEventListener('input', function (e) {
-  console.log('img url input');
   if (validURL(imgUrl.value)) {
+
     placeholderImg.src = imgUrl.value;
-    console.log(imgUrl.value);
   }
 });
 
 /* getting form elements */
-var title = document.getElementById('title');
-console.log(title);
-var notes = document.getElementById('notes');
-console.log(notes);
+
+var form = document.getElementById('form');
 
 /* adding event listener for submit and setting input values */
-document.addEventListener('submit', function (event) {
-  console.log('Submit Event');
+form.addEventListener('submit', function (event) {
 
   event.preventDefault();
 
+  /* if an entry is being edited, match the id and change the values to user input */
+  if (data.editing !== null) {
+
+    for (var i = 0; i < data.entries.length; i++) {
+
+      if (data.editing.entryId === data.entries[i].entryId) {
+
+        data.editing.title = form.elements.title.value;
+
+        data.editing.notes = form.elements.notes.value;
+
+        data.editing.image = imgUrl.value;
+
+      }
+    }
+    showEntries();
+    return data.editing;
+  }
   /* New UserInput Object */
   var userInput = {
     title: '',
@@ -63,55 +71,18 @@ document.addEventListener('submit', function (event) {
   };
 
   /* UserInput Object values are set by input */
-  userInput.title = title.value;
-  console.log('userInput.title', userInput.title);
-  console.log('title.value', title.value);
+  userInput.title = form.elements.title.value;
 
-  userInput.notes = notes.value;
-  console.log('userInput.notes', userInput.notes);
-  console.log('notes.value', notes.value);
+  userInput.notes = form.elements.notes.value;
 
   userInput.image = imgUrl.value;
-  console.log('userInput.image', userInput.image);
-  console.log('imgUrl.value', imgUrl.value);
 
   userInput.entryId = data.nextEntryId;
-  console.log('userInput.entryId', userInput.entryId);
-  console.log('data.nextEntryId', data.nextEntryId);
 
-  /* if an entry is being edited, match the id and change the values to user input */
-  console.log('data.editing', data.editing);
-  if (data.editing !== null) {
-    console.log('data.editing', data.editing);
-    for (var i = 0; i < data.entries.length; i++) {
-      console.log('data.entries.length', data.entries.length);
-      console.log('data.entries[i].entryId', data.entries[i].entryId);
-
-      if (data.editing === data.entries[i].entryId) {
-        data.entries[i].title = title.value;
-        console.log('data.entries[i].title', data.entries[i].title);
-        console.log('title.value', title.value);
-
-        data.entries[i].notes = notes.value;
-        console.log('data.entries[i].notes', data.entries[i].notes);
-        console.log('notes.value', notes.value);
-
-        data.entries[i].image = imgUrl.value;
-        console.log('data.entries[i].image', data.entries[i].image);
-        console.log('imgUrl.value', imgUrl.value);
-      }
-    }
-    return;
-  } else {
   /* data object is updated with the new entry */
-    console.log('data.nextEntryId', data.nextEntryId);
-    data.nextEntryId++;
-    console.log('data.nextEntryId', data.nextEntryId);
+  data.nextEntryId++;
 
-    console.log('data.entries', data.entries);
-    data.entries.unshift(userInput);
-    console.log('data.entries', data.entries);
-  }
+  data.entries.unshift(userInput);
 
   /* storing input values locally */
   var inputToJSON = JSON.stringify(data);
@@ -129,11 +100,9 @@ var entryContainer = document.getElementById('entry-list');
 var placeholder = document.getElementById('no-entry');
 
 function loadEntries(data) {
-  console.log('loadEntries()');
 
-  console.log('data.entries.length', data.entries.length);
   if (data.entries.length === 0) {
-    console.log('data.entries.length', data.entries.length);
+
     entryContainer.style.display = 'none';
     placeholder.style.display = '';
   } else {
@@ -142,18 +111,13 @@ function loadEntries(data) {
   }
   for (var i = data.entries.length - 1; i >= 0; i--) {
     var newEntry = addEntry(data.entries[i]);
-    console.log('data.entries[i]', data.entries[i]);
-    console.log('newEntry', newEntry);
 
-    console.log('entryContainer', entryContainer);
     entryContainer.prepend(newEntry);
-    console.log('entryContainer', entryContainer);
   }
 }
 
 /* This function turns UserInput into a DOM elements */
 function addEntry(entry) {
-  console.log('addEntry()');
 
   var newEntryRow = document.createElement('div');
   newEntryRow.className = 'entries-row';
@@ -169,25 +133,20 @@ function addEntry(entry) {
 
   var title = document.createElement('h2');
   title.className = 'entries-title';
-  title.textContent = entry.notes;
-  console.log('title', title);
+  title.textContent = entry.title;
 
   var imgURL = document.createElement('div');
   imgURL.className = 'img-wrapper1';
   imgURL.style.backgroundImage = 'url(' + entry.image + ')';
-  console.log('imgUrl', imgUrl);
 
   var entryNotes = document.createElement('p');
   entryNotes.textContent = entry.notes;
   entryNotes.className = 'entries-text';
-  console.log('entryNotes', entryNotes);
 
   var editPen = document.createElement('img');
   editPen.src = 'images/purpPen.svg';
   editPen.className = 'absoluteImg';
   editPen.setAttribute('entryId', entry.entryId);
-  console.log('editPen', editPen);
-
   /* appends the new elements to their parent element */
   newEntryCol.appendChild(title);
   newEntryCol.appendChild(editPen);
@@ -200,8 +159,6 @@ function addEntry(entry) {
   newEntry.className = 'entry';
   newEntry.appendChild(newEntryRow);
 
-  console.log('newEntry', newEntry);
-
   if (placeholder) {
     placeholder.style.display = 'none';
   }
@@ -210,33 +167,25 @@ function addEntry(entry) {
 }
 
 entryContainer.addEventListener('click', function (event) {
-  console.log('Click Event');
 
-  console.log('event.target.className', event.target.className);
   if (event.target.className !== 'absoluteImg') {
     return false;
   } else {
     for (var i = data.entries.length - 1; i >= 0; i--) {
-      console.log('data.entries[i].entryId', data.entries[i].entryId);
-      console.log(event.target.getAttribute('entryId'));
 
       if (data.entries[i].entryId === parseInt(event.target.getAttribute('entryId'))) {
-        title.value = data.entries[i].title;
-        console.log('title.value', title.value);
-        console.log('data.entries[i].title', data.entries[i].title);
+        form.elements.title.value = data.entries[i].title;
 
+        placeholderImg.src = data.entries[i].image;
         imgUrl.value = data.entries[i].image;
-        console.log('imgUrl.value', imgUrl.value);
-        console.log('data.entries[i].image', data.entries[i].image);
 
-        notes.value = data.entries[i].notes;
-        console.log('notes.value', notes.value);
-        console.log('data.entries[i].notes', data.entries[i].notes);
+        form.elements.notes.value = data.entries[i].notes;
 
-        showEntryForm();
-        console.log('data.editing', data.editing);
         data.editing = data.entries[i];
-        console.log('data.editing', data.editing);
+        var inputToJSON = JSON.stringify(data);
+        localStorage.setItem('javascript-local-storage', inputToJSON);
+        showEntryForm();
+
       }
     }
   }
@@ -244,26 +193,17 @@ entryContainer.addEventListener('click', function (event) {
 
 /* click event on anchor to change views to entries */
 var entriesTab = document.querySelector('.entries-anchor');
-console.log('entriesTab', entriesTab);
 
 var entries = document.querySelector('[data-view="entries"]');
-console.log('entries', entries);
 
 var entryForm = document.querySelector('[data-view="entry-form"]');
-console.log('entryForm', entryForm);
 
 /* click event on nav item for entries page view */
 function showEntries() {
-  console.log('showEntries()');
 
-  console.log('entryContainer', entryContainer);
   entryContainer.innerHTML = '';
-  console.log('entryContainer', entryContainer);
-  /*
+
   loadEntries(data);
-  */
-  console.log('loadEntries(data)', loadEntries(data));
-  console.log('entryContainer', entryContainer);
 
   data.view = 'entries';
   entryForm.className = 'view-hidden';
@@ -282,7 +222,6 @@ entriesTab.addEventListener('click', function () {
 
 /* click event on new button to return to input form */
 function showEntryForm() {
-  console.log('showEntryForm()');
 
   data.view = 'entry-form';
 
@@ -298,5 +237,7 @@ function showEntryForm() {
 var newButton = document.getElementById('newEntryButton');
 
 newButton.addEventListener('click', function () {
+  form.reset();
+  placeholderImg.src = 'images/placeholder-image-square.jpg';
   showEntryForm();
 });
